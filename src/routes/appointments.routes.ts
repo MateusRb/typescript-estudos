71bd0c6ebf3e100a330fa3982a8496338c9f8039
Mateus Ribeiro <mateus_ribeiro21@hotmail.com>
2020-06-08 20:41:17 -0300

    refs adicionando autenticação nas rotas da aplicação

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm'
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService'
 
+import ensureAutheticated from '../middlewares/ensureAuthenticated'
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAutheticated)
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
@@ -16,7 +21,6 @@ appointmentsRouter.get('/', async (request, response) => {
 appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider_id, date } = request.body;
-
     const parsedDate = parseISO(date);
   
     const createAppointment = new CreateAppointmentService()
